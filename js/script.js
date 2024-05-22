ad25d1c8c92f663a6e12a089f8c2840f5daae7f1
lazzaric92 <lazzaric_92@hotmail.it>
2024-05-22 16:31:29 +0200

    created array 'ourTeam'

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const ourTeam = [
+    {
+        name: 'Wayne Barnett',
+        'job-position': 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        'job-position': 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        'job-position': 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        'job-position': 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        'job-position': 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        'job-position': 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
+
